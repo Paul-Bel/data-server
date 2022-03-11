10fd68f79665901334c048665a8a3dc2420dfc3b
Paul-Bel <pn.beloborodov.msk@gmail.com>
2022-03-11 15:41:53 +0300

    added auth

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
+const low = require('lowdb');
+const db = low('server-data.json');
 const router = jsonServer.router('server-data.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
+
+server.get('/login', (req, res) => {
+    const user = db.get("users")
+        .find({ name: "roy"})
+    if (user) {
+        res.json(user)
+    } else {
+        res.sendStatus(404)
+    }
+})
 
 server.listen(port);
